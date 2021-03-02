3a74a97e6ce5ca466a65111db726b4cbed5eae2d
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-03-02 07:57:46 -0300

    02/03/2021 - 07:57

---
+++ codigo/maiorValorN.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+Fonte: https://docs.google.com/presentation/d/177g9N4hFAddzrY8drxVk1CDRtZ9ywSQB9KhqgymPGe0/edit#slide=id.gc0f05f94a3_0_64
+Escreva um programa em C que leia n valores inteiros positivos e 
+mostre o maior valor
+'''
+
+
---
+++ codigo/tabuadaN.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+Fonte: https://docs.google.com/presentation/d/177g9N4hFAddzrY8drxVk1CDRtZ9ywSQB9KhqgymPGe0/edit#slide=id.gc0f05f94a3_0_64
+Escreva um programa em C que mostre a tabuada de um número fornecido 
+pelo usuário. (considerar a tabuada do número 1 ao 10).
+'''
+
+
